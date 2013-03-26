a51f28339f22c5ffb98519e479251f5ae4f675b0
di98jgu <di98jgu@gamil.com>
2013-03-26 12:46:37 +0100

    Merge conflict in DicNixApp

--- DicNix/src/se/ltu/dicnix/LocationDetailsActivity.java
+++ DicNix/src/se/ltu/dicnix/LocationDetailsActivity.java
@@ -18,9 +18,6 @@
 
 package se.ltu.dicnix;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -39,24 +36,18 @@ import database.Snowsensor;
  */
 public class LocationDetailsActivity extends Activity {
 	
-	/** Location details id tag for use in communication between activity's */
+	/** Location details id tag for use in communication between activities */
 	public final static String ID_LOCATION_DETAILS = "dicnix.id_location_details";
-	public final static String[] columns1 = {Snowsensor.ID, Snowsensor.TIMESTAMP, Snowsensor.NAME, Snowsensor.LOCATION, Snowsensor.LATITUDE, Snowsensor.LONGITUDE, Snowsensor.TYPENAME, Snowsensor.DEPLOYEDSTATE, Snowsensor.INFO, Snowsensor.DOMAIN, Snowsensor.CREATED, Snowsensor.UPDATED};
-	public final static String[] columns2 = {Snowdata.VISIBILITY, Snowdata.SHOVELED, Snowdata.WEIGHT, Snowdata.DEPTH, Snowdata.TEMPERATURE, Snowdata.HUMIDITY, Snowdata.DATA_TIME, Snowdata.SERIAL};
+	public final static String[] columns1 = {Snowsensor.ID, Snowsensor.SERIAL, Snowsensor.NAME, Snowsensor.LOCATION, Snowsensor.LATITUDE, Snowsensor.LONGITUDE, Snowsensor.TYPENAME, Snowsensor.DEPLOYEDSTATE, Snowsensor.VISIBILITY, Snowsensor.INFO, Snowsensor.DOMAIN, Snowsensor.CREATED, Snowsensor.UPDATED};
+	public final static String[] columns2 = {Snowdata.ID, Snowdata.SERIAL, Snowdata.INFO, Snowdata.SHOVELED, Snowdata.WEIGHT, Snowdata.DEPTH, Snowdata.TEMPERATURE, Snowdata.HUMIDITY, Snowdata.DATA_TIME, Snowdata.TIMESTAMP};
 
     Cursor returnedCursor1 = null;
     Cursor returnedCursor2 = null;
 	
-	ContentValues cv1 = new ContentValues(12);
-	ContentValues cv2 = new ContentValues(8);
+	ContentValues cv1 = new ContentValues(13);
+	ContentValues cv2 = new ContentValues(10);
 	
-	boolean database_data_inserted = false;
-	
-	List<ssc.Sensor> sensorList = new ArrayList<ssc.Sensor>();
-	List<ssc.SnowPressure> readingsList = new ArrayList<ssc.SnowPressure>();
-	ssc.Sensor sensor = null;
-	ssc.SnowPressure readings = null;
-	
+
 	protected DicNixApp application;
 	
 	/**
@@ -75,94 +66,23 @@ public class LocationDetailsActivity extends Activity {
       String sensor_serial = i.getStringExtra("sensor_serial");
       String location_id = String.valueOf(clicked_item_id);      
  
-      
-      /**
-       * Get sensors
-       */
-      sensorList = application.getSensors();
-     
-      /**
-       * Create database
-       */
-      Snowsensor SS = new Snowsensor(getApplicationContext());
-      Snowdata SD = new Snowdata(getApplicationContext());
            
       /**
-       * Open database
+       * Create columns for database
        */
-      SS.open();
-      SD.open();
-
-      
-          /**
-           * Create columns for database
-           */
-          SS.setColumns(columns1);  
-          SD.setColumns(columns2);  
+      application.snowsensor.setColumns(columns1);  
+      application.snowdata.setColumns(columns2);  
     	  
-	      /**
-	       * Insert remote database data into local database
-	       */
-	      for (int k = 0; k < sensorList.size(); k++) {
-	    	  readingsList = application.getReadings(sensorList.get(k));
-	          readings = readingsList.get(0);
-	    	  
-	          cv1.put(columns1[2], sensorList.get(k).getName());
-	          cv1.put(columns1[3], sensorList.get(k).getLocation());
-	          cv1.put(columns1[4], String.valueOf(sensorList.get(k).getLatitude()));
-	          cv1.put(columns1[5], String.valueOf(sensorList.get(k).getLongitude()));
-	          cv1.put(columns1[6], sensorList.get(k).getTypeName());
-	          cv1.put(columns1[7], sensorList.get(k).getDeployedState());
-	          cv1.put(columns1[8], sensorList.get(k).getInfo());
-	          cv1.put(columns1[9], sensorList.get(k).getDomain());
-	          cv1.put(columns1[10], String.valueOf(sensorList.get(k).getCreated()));
-	          cv1.put(columns1[11], String.valueOf(sensorList.get(k).getUpdated()));
-	          
-	          cv2.put(columns2[0], sensorList.get(k).getVisibility());
-	          cv2.put(columns2[1], readings.getShoveld());
-	          cv2.put(columns2[2], readings.getWeight());
-	          cv2.put(columns2[3], readings.getDepth());
-	          cv2.put(columns2[4], readings.getTemperature());
-	          cv2.put(columns2[5], readings.getHumidity());
-	          cv2.put(columns2[6], String.valueOf(readings.getDataTime()));
-	          cv2.put(columns2[7], sensorList.get(k).getSerial());
-	
-	          
-	          SS.insert(cv1);
-	          SD.insert(cv2);
-
-	      }
-      
-      
-      
-      /**
-       * Determine sensor
-       */
-      for (int j = 0; j < sensorList.size(); j++) {
-    	  String temp_sensor = sensorList.get(j).getSerial();
-    	  if (temp_sensor.equals(sensor_serial)) {
-    		  sensor = sensorList.get(j);
-        	  readingsList = application.getReadings(sensor);
-    	      readings = readingsList.get(0);
-    		  break;
-    	  }
-      }
-      
+    
       
       /**
        * Query database for wanted information
        */
-      returnedCursor1 = SS.select("latitude = " + String.valueOf(sensor.getLatitude()), null);
-      returnedCursor2 = SD.select("temperature = " + String.valueOf(readings.getTemperature()), null);
+      returnedCursor1 = application.snowsensor.select("serial = '" + sensor_serial + "'", null);
+      returnedCursor2 = application.snowdata.select("serial = '" + sensor_serial + "'", null);
       
       LocationDetailsAdp details = new LocationDetailsAdp(returnedCursor1, returnedCursor2);
       details.populate(location_id);   
-
-      /**
-       * Close database
-       */
-      SS.close();
-      SD.close();
       
    }
 	
@@ -198,7 +118,6 @@ public class LocationDetailsActivity extends Activity {
 	      private TextView domain = null;
 	      private TextView created = null;
 	      private TextView updated = null;
-	      private TextView id = null;
 	      private TextView shoveled = null;
 	      private TextView weight = null;
 	      private TextView depth = null;
@@ -230,7 +149,6 @@ public class LocationDetailsActivity extends Activity {
 	         domain = (TextView) findViewById(R.id.domain);
 	         created = (TextView) findViewById(R.id.created);
 	         updated = (TextView) findViewById(R.id.updated);
-	         id = (TextView) findViewById(R.id.location_id);
 	         shoveled = (TextView) findViewById(R.id.shoveled);
 	         weight = (TextView) findViewById(R.id.weight);
 	         depth = (TextView) findViewById(R.id.depth);
@@ -252,6 +170,7 @@ public class LocationDetailsActivity extends Activity {
 	         returnedCursor2.moveToFirst();
 	       
 	         img.setImageResource(R.drawable.igloo);
+	         serial.setText(returnedCursor1.getString(returnedCursor1.getColumnIndex("serial")));
 	         name.setText(returnedCursor1.getString(returnedCursor1.getColumnIndex("name")));
 	         location.setText(returnedCursor1.getString(returnedCursor1.getColumnIndex("location")));
 	         latitude.setText(returnedCursor1.getString(returnedCursor1.getColumnIndex("latitude")));
@@ -262,16 +181,15 @@ public class LocationDetailsActivity extends Activity {
 	         domain.setText(returnedCursor1.getString(returnedCursor1.getColumnIndex("domain")));
 	         created.setText(returnedCursor1.getString(returnedCursor1.getColumnIndex("created")));
 	         updated.setText(returnedCursor1.getString(returnedCursor1.getColumnIndex("updated")));
-
-	         visibility.setText(returnedCursor2.getString(returnedCursor2.getColumnIndex("visibility")));
+	         visibility.setText(returnedCursor1.getString(returnedCursor1.getColumnIndex("visibility")));
+	         
 	         shoveled.setText(returnedCursor2.getString(returnedCursor2.getColumnIndex("shoveled")));
 	         weight.setText(returnedCursor2.getString(returnedCursor2.getColumnIndex("weight")));
 	         depth.setText(returnedCursor2.getString(returnedCursor2.getColumnIndex("depth")));
 	         temperature.setText(returnedCursor2.getString(returnedCursor2.getColumnIndex("temperature")));
 	         humidity.setText(returnedCursor2.getString(returnedCursor2.getColumnIndex("humidity")));
 	         data_time.setText(returnedCursor2.getString(returnedCursor2.getColumnIndex("data_time")));
-	         serial.setText(returnedCursor2.getString(returnedCursor2.getColumnIndex("serial")));
-	         
+	       	         
 	         
          return;
 
